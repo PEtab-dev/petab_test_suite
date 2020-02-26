1127ce95c18d609c2347c4bae749206b2a650d8d
yannikschaelte <yannik.schaelte@gmail.com>
2020-02-26 14:56:35 +0100

    add simulations

--- cases/0001/0001.py
+++ cases/0001/0001.py
@@ -1,13 +1,16 @@
 from petabtests import *
 from petab.C import *
+
 import pandas as pd
 
-"""Test something"""
+
+# problem setup
+
 test_id = 1
 
 condition_df = pd.DataFrame(data={
-    CONDITION_ID: ['c0', 'c1'],
-    'a0': [2, 3],
+    CONDITION_ID: ['c0'],
+    'a0': [1],
 }).set_index([CONDITION_ID])
 
 measurement_df = pd.DataFrame(data={
@@ -28,14 +31,24 @@ parameter_df = pd.DataFrame(data={
     PARAMETER_SCALE: [LIN] * 3,
     LOWER_BOUND: [0] * 3,
     UPPER_BOUND: [10] * 3,
-    NOMINAL_VALUE: [1] * 3,
+    NOMINAL_VALUE: [0, 0.8, 0.6],
     ESTIMATE: [1] * 3,
 })
 
-write_files(test_id=test_id,
-            condition_dfs=[condition_df],
-            measurement_dfs=[measurement_df],
-            observable_dfs=[observable_df],
-            parameter_df=parameter_df)
 
-# TODO: write test results
+# test results
+
+simulation_df = measurement_df.copy(deep=True).rename(
+    columns={MEASUREMENT: SIMULATION})
+simulation_df[SIMULATION] = [analytical_b(t, 1, 0, 0.8, 0.6) \
+                             for t in simulation_df[TIME]]
+
+
+# write files
+
+write_files(test_id=test_id,
+            parameter_df=parameter_df,
+            condition_dfs=[condition_df],
+            observable_dfs=[observable_df],
+            measurement_dfs=[measurement_df],
+            simulation_dfs=[simulation_df])
--- petabtests/__init__.py
+++ petabtests/__init__.py
@@ -7,6 +7,8 @@ from petab.C import *
 import yaml
 from shutil import copyfile
 
+from .model import *
+
 
 def yaml_name(_id: Union[int, str]) -> str:
     return test_id_str(_id) + '.yaml'
@@ -16,24 +18,12 @@ def test_id_str(_id: str) -> str:
     return f"{_id:0>4}"
 
 
-"""Analytical model"""
-
-
-def analytical_a(t, a0, b0, k1, k2):
-    return k2 * (a0 + b0) / (k1 + k2) \
-           + (a0 - k2 * (a0 + b0) / (k1 + k2)) * np.exp(-(k1 + k2) * t)
-
-
-def analytical_b(t, a0, b0, k1, k2):
-    return k1 * (a0 + b0) / (k1 + k2) \
-           + (b0 - k1 * (a0 + b0) / (k1 + k2)) * np.exp(-(k1 + k2) * t)
-
-
 def write_files(test_id,
-                condition_dfs: List[pd.DataFrame],
-                measurement_dfs: List[pd.DataFrame],
-                observable_dfs: List[pd.DataFrame],
-                parameter_df: pd.DataFrame) -> None:
+                parameter_df: pd.DataFrame = None,
+                condition_dfs: List[pd.DataFrame] = None,
+                observable_dfs: List[pd.DataFrame] = None,
+                measurement_dfs: List[pd.DataFrame] = None,
+                simulation_dfs: pd.DataFrame = None) -> None:
     id_str = test_id_str(test_id)
     # petab yaml
     model_name = 'model.xml'
@@ -50,27 +40,35 @@ def write_files(test_id,
         ]
     }
 
-    for name, writer, dfs in zip(
-            ['conditions', 'measurements', 'observables'],
-            [petab.write_condition_df, petab.write_measurement_df,
-             petab.write_observable_df],
-            [condition_dfs, measurement_dfs, observable_dfs]):
-        for idx, df in enumerate(dfs):
-            if len(dfs) == 1:
-                idx = ''
-            fname = f"{name}{idx}.tsv"
-            writer(df, os.path.join('cases', id_str, fname))
-            config[PROBLEMS][0][name[0:-1] + '_files'].append(fname)
-
+    # parameters
     fname = f"parameters.tsv"
     petab.write_parameter_df(parameter_df,
                              os.path.join('cases', id_str, fname))
     config[PARAMETER_FILE] = fname
 
+    # model
     copyfile(os.path.join('petabtests', 'conversion.xml'),
              os.path.join('cases', id_str, model_name))
 
+    # conditions, observables, measurements, simulations
+    for name, writer, dfs in zip(
+            ['conditions', 'measurements', 'observables', 'simulations'],
+            [petab.write_condition_df, petab.write_measurement_df,
+             petab.write_observable_df, petab.write_measurement_df],
+            [condition_dfs, measurement_dfs, observable_dfs, simulation_dfs]):
+        if not dfs:
+            continue
+        for idx, df in enumerate(dfs):
+            if len(dfs) == 1:
+                idx = ''
+            fname = f"{name}{idx}.tsv"
+            writer(df, os.path.join('cases', id_str, fname))
+            if name != 'simulations':
+                config[PROBLEMS][0][name[0:-1] + '_files'].append(fname)
+
+    # validate petab
     petab.validate(config, path_prefix=os.path.join('cases', id_str))
 
+    # yaml
     with open(os.path.join('cases', id_str, yaml_fname), 'w') as outfile:
         yaml.dump(config, outfile, default_flow_style=False)
