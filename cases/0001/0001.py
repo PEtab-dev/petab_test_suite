ab73f2c71e850ad8a7cde446bb91eabca4de41aa
yannikschaelte <yannik.schaelte@gmail.com>
2020-02-26 15:45:20 +0100

    add solutions yaml

--- cases/0001/0001.py
+++ cases/0001/0001.py
@@ -4,7 +4,7 @@ from petab.C import *
 import pandas as pd
 
 
-# problem setup
+# problem --------------------------------------------------------------------
 
 test_id = 1
 
@@ -35,8 +35,15 @@ parameter_df = pd.DataFrame(data={
     ESTIMATE: [1] * 3,
 })
 
+# write files
 
-# test results
+write_problem(test_id=test_id,
+              parameter_df=parameter_df,
+              condition_dfs=[condition_df],
+              observable_dfs=[observable_df],
+              measurement_dfs=[measurement_df])
+
+# results --------------------------------------------------------------------
 
 simulation_df = measurement_df.copy(deep=True).rename(
     columns={MEASUREMENT: SIMULATION})
@@ -46,9 +53,7 @@ simulation_df[SIMULATION] = [analytical_b(t, 1, 0, 0.8, 0.6) \
 
 # write files
 
-write_files(test_id=test_id,
-            parameter_df=parameter_df,
-            condition_dfs=[condition_df],
-            observable_dfs=[observable_df],
-            measurement_dfs=[measurement_df],
-            simulation_dfs=[simulation_df])
+write_solution(test_id=test_id,
+               llh=None,
+               chi2=None,
+               simulation_dfs=[simulation_df])
--- petabtests/__init__.py
+++ petabtests/__init__.py
@@ -9,25 +9,34 @@ from shutil import copyfile
 
 from .model import *
 
+# constant definitions
+LLH = 'llh'
+CHI2 = 'chi2'
+SIMULATION_FILES = 'simulation_files'
 
-def yaml_name(_id: Union[int, str]) -> str:
+
+def problem_yaml_name(_id: Union[int, str]) -> str:
     return test_id_str(_id) + '.yaml'
+
+
+def solution_yaml_name(_id: Union[int, str]) -> str:
+    return test_id_str(_id) + '_solution.yaml'
 
 
 def test_id_str(_id: str) -> str:
     return f"{_id:0>4}"
 
 
-def write_files(test_id,
-                parameter_df: pd.DataFrame = None,
-                condition_dfs: List[pd.DataFrame] = None,
-                observable_dfs: List[pd.DataFrame] = None,
-                measurement_dfs: List[pd.DataFrame] = None,
-                simulation_dfs: pd.DataFrame = None) -> None:
+def write_problem(test_id,
+                  parameter_df: pd.DataFrame,
+                  condition_dfs: List[pd.DataFrame],
+                  observable_dfs: List[pd.DataFrame],
+                  measurement_dfs: List[pd.DataFrame]) -> None:
     id_str = test_id_str(test_id)
+    model_name = 'model.xml'  # TODO allow passing a model
+    yaml_fname = problem_yaml_name(test_id)
+
     # petab yaml
-    model_name = 'model.xml'
-    yaml_fname = yaml_name(test_id)
     config = {
         FORMAT_VERSION: petab.__format_version__,
         PROBLEMS: [
@@ -52,19 +61,11 @@ def write_files(test_id,
 
     # conditions, observables, measurements, simulations
     for name, writer, dfs in zip(
-            ['conditions', 'measurements', 'observables', 'simulations'],
+            ['conditions', 'measurements', 'observables'],
             [petab.write_condition_df, petab.write_measurement_df,
-             petab.write_observable_df, petab.write_measurement_df],
-            [condition_dfs, measurement_dfs, observable_dfs, simulation_dfs]):
-        if not dfs:
-            continue
-        for idx, df in enumerate(dfs):
-            if len(dfs) == 1:
-                idx = ''
-            fname = f"{name}{idx}.tsv"
-            writer(df, os.path.join('cases', id_str, fname))
-            if name != 'simulations':
-                config[PROBLEMS][0][name[0:-1] + '_files'].append(fname)
+             petab.write_observable_df],
+            [condition_dfs, measurement_dfs, observable_dfs]):
+        _write_dfs_to_files(id_str, name, writer, dfs, config[PROBLEMS][0])
 
     # validate petab
     petab.validate(config, path_prefix=os.path.join('cases', id_str))
@@ -72,3 +73,37 @@ def write_files(test_id,
     # yaml
     with open(os.path.join('cases', id_str, yaml_fname), 'w') as outfile:
         yaml.dump(config, outfile, default_flow_style=False)
+
+
+def write_solution(test_id,
+                   simulation_dfs: List[pd.DataFrame],
+                   chi2, llh):
+    id_str = test_id_str(test_id)
+    yaml_fname = solution_yaml_name(test_id)
+
+    # solution yaml
+    config = {
+        FORMAT_VERSION: petab.__format_version__,
+        CHI2: chi2,
+        LLH: llh,
+        SIMULATION_FILES: [],
+    }
+
+    # simulations
+    _write_dfs_to_files(
+        id_str, "simulations", petab.write_measurement_df,
+        simulation_dfs, config)
+
+    with open(os.path.join('cases', id_str, yaml_fname), 'w') as outfile:
+        yaml.dump(config, outfile, default_flow_style=False)
+
+
+def _write_dfs_to_files(id_str, name, writer, dfs, config):
+    """Write data frames to files and add them to config."""
+    for idx, df in enumerate(dfs):
+        if len(dfs) == 1:
+            idx = ''
+        fname = f"{name}{idx}.tsv"
+        writer(df, os.path.join('cases', id_str, fname))
+        if config:
+            config[name[0:-1] + '_files'].append(fname)
