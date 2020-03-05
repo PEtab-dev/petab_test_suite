ef47a40b6a4f9f73784a4832fed578ccf2b4269f
yannikschaelte <yannik.schaelte@gmail.com>
2020-03-05 16:18:06 +0100

    add test for parameterized initial conditions

--- cases/0013/0013.py
+++ cases/0013/0013.py
@@ -13,29 +13,29 @@ model = DEFAULT_MODEL_FILE
 
 condition_df = pd.DataFrame(data={
     CONDITION_ID: ['c0'],
+    'B': ['par'],
 }).set_index([CONDITION_ID])
 
 measurement_df = pd.DataFrame(data={
     OBSERVABLE_ID: ['obs_a', 'obs_a'],
     SIMULATION_CONDITION_ID: ['c0', 'c0'],
     TIME: [0, 10],
-    MEASUREMENT: [0.7, 0.1],
-    NOISE_PARAMETERS: ['0.5;2', '0.5;2']
+    MEASUREMENT: [0.7, 0.1]
 })
 
 observable_df = pd.DataFrame(data={
     OBSERVABLE_ID: ['obs_a'],
     OBSERVABLE_FORMULA: ['A'],
-    NOISE_FORMULA: ['noiseParameter1_obs_a + noiseParameter2_obs_a']
+    NOISE_FORMULA: [0.5]
 }).set_index([OBSERVABLE_ID])
 
 parameter_df = pd.DataFrame(data={
-    PARAMETER_ID: ['a0', 'b0', 'k1', 'k2'],
-    PARAMETER_SCALE: [LIN] * 4,
-    LOWER_BOUND: [0] * 4,
-    UPPER_BOUND: [10] * 4,
-    NOMINAL_VALUE: [1, 0, 0.8, 0.6],
-    ESTIMATE: [1] * 4,
+    PARAMETER_ID: ['k1', 'k2', 'par'],
+    PARAMETER_SCALE: [LIN] * 3,
+    LOWER_BOUND: [0] * 3,
+    UPPER_BOUND: [10] * 3,
+    NOMINAL_VALUE: [0.8, 0.6, 7],
+    ESTIMATE: [1] * 3,
 }).set_index(PARAMETER_ID)
 
 
@@ -45,13 +45,16 @@ write_problem(test_id=test_id,
               parameter_df=parameter_df,
               condition_dfs=[condition_df],
               observable_dfs=[observable_df],
-              measurement_dfs=[measurement_df])
+              measurement_dfs=[measurement_df],
+              sbml_files=['conversion_modified.xml'])
 
 # solutions ------------------------------------------------------------------
 
 simulation_df = measurement_df.copy(deep=True).rename(
     columns={MEASUREMENT: SIMULATION})
-simulation_df[SIMULATION] = [analytical_a(t, 1, 0, 0.8, 0.6)
+# in the model, concentrations are used, which do not depend on the
+#  compartment size, so that the species values should stay the same
+simulation_df[SIMULATION] = [analytical_a(t, 1, 7, 0.8, 0.6)
                              for t in simulation_df[TIME]]
 
 chi2 = petab.calculate_chi2(
