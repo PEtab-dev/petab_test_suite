1e09b971a60fe13a42d002019d612ed42682f9d2
Yannik Schälte <31767307+yannikschaelte@users.noreply.github.com>
2020-03-10 13:12:06 +0100

    add versioning, add proper license (#16)
    * add versioning, add proper LICENSE
    
    * fix flake
    
    * undo license change

--- setup.py
+++ setup.py
@@ -5,18 +5,27 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
+
+def read(fname):
+    with open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+long_description = read(path.join(here, 'README.md'))
+
+# Get the version
+exec(read(path.join(here, 'petabtests', 'version.py')))
 
 setup(
     name='petabtests',
-    version='0.0.0a1',
+    version=__version__,  # noqa: F821
     description='PEtab testsuite library',
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/PEtab-dev/petab_test_suite',
     author='PEtab-dev',
+    author_email='yannik.schaelte@gmail.com',
     # author_email='',
     classifiers=[
         # How mature is this project? Common values are
@@ -40,7 +49,7 @@ setup(
     packages=find_packages(where='petab_test_suite'),
     install_requires=['numpy',
                       'pandas',
-                      'petab>=0.1.3'],
+                      'petab>=0.1.4'],
     python_requires='>=3.6',
     # install_requires=[''],
     entry_points={
