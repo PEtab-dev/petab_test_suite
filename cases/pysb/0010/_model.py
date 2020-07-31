991ec989775bcaf322e28b574814af8b04cc857c
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-07-31 14:05:55 +0200

    Change species names to have A,B for pysb-observable IDs
    so we can reuse the observable formulas without modification

--- cases/pysb/0010/_model.py
+++ cases/pysb/0010/_model.py
@@ -1,18 +1,21 @@
-from pysb import Model, Monomer, Parameter, Compartment, Rule, Initial
+from pysb import Model, Monomer, Parameter, Compartment, Rule, Initial, Observable
 
 Model()
 
 Compartment('compartment')
 
-Monomer('A')
-Monomer('B')
+Monomer('A_')
+Monomer('B_')
 
 Parameter('a0', 1.0)
 Parameter('b0', 1.0)
 Parameter('k1', 0.0)
 Parameter('k2', 0.0)
 
-Rule('conversion', A() ** compartment | B() ** compartment, k1, k2)
+Rule('conversion', A_() ** compartment | B_() ** compartment, k1, k2)
 
-Initial(A() ** compartment, a0)
-Initial(B() ** compartment, b0)
+Initial(A_() ** compartment, a0)
+Initial(B_() ** compartment, b0)
+
+Observable("A", A_())
+Observable("B", B_())
--- petabtests/conversion_pysb.py
+++ petabtests/conversion_pysb.py
@@ -1,23 +1,21 @@
-from pysb import Model, Monomer, Parameter, Compartment, Rule, Initial, Observable, Expression
+from pysb import Model, Monomer, Parameter, Compartment, Rule, Initial, Observable
 
 Model()
 
 Compartment('compartment')
 
-Monomer('A')
-Monomer('B')
+Monomer('A_')
+Monomer('B_')
 
 Parameter('a0', 1.0)
 Parameter('b0', 1.0)
 Parameter('k1', 0.0)
 Parameter('k2', 0.0)
 
-Rule('conversion', A() ** compartment | B() ** compartment, k1, k2)
+Rule('conversion', A_() ** compartment | B_() ** compartment, k1, k2)
 
-Initial(A() ** compartment, a0)
-Initial(B() ** compartment, b0)
+Initial(A_() ** compartment, a0)
+Initial(B_() ** compartment, b0)
 
-Observable("_obs_a", A)
-Expression("obs_a", _obs_a)
-Observable("_obs_b", B)
-Expression("obs_b", _obs_b)
+Observable("A", A_())
+Observable("B", B_())
