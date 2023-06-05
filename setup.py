abfbc8bc6eb5ca8559bd6990f9c3ede1fdc82738
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2023-06-05 13:37:46 +0200

    python>=3.9

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'pysb',
         'simplesbml',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.9',
     entry_points={
         'console_scripts': [
             'petabtests_create = petabtests.core:create',
