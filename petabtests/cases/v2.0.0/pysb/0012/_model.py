3e2cba487ae5e508034479f50b0e8c965d933c8f
Daniel Weindl <daniel.weindl@helmholtz-munich.de>
2024-07-01 11:35:40 +0200

    ..

--- petabtests/cases/v2.0.0/pysb/0012/_model.py
+++ petabtests/cases/v2.0.0/pysb/0012/_model.py
@@ -1,18 +1,26 @@
-from pysb import Model, Monomer, Parameter, Compartment, Rule, Initial, Observable
+from pysb import (
+    Model,
+    Monomer,
+    Parameter,
+    Compartment,
+    Rule,
+    Initial,
+    Observable,
+)
 
 Model()
 
-Compartment('compartment')
+Compartment("compartment")
 
-Monomer('A_')
-Monomer('B_')
+Monomer("A_")
+Monomer("B_")
 
-Parameter('a0', 1.0)
-Parameter('b0', 1.0)
-Parameter('k1', 0.0)
-Parameter('k2', 0.0)
+Parameter("a0", 1.0)
+Parameter("b0", 1.0)
+Parameter("k1", 0.0)
+Parameter("k2", 0.0)
 
-Rule('conversion', A_() ** compartment | B_() ** compartment, k1, k2)
+Rule("conversion", A_() ** compartment | B_() ** compartment, k1, k2)
 
 Initial(A_() ** compartment, a0)
 Initial(B_() ** compartment, b0)
