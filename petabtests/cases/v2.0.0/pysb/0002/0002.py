5e29d3e3736df7f41b0c04246a656935a5749f30
Daniel Weindl <daniel.weindl@uni-bonn.de>
2024-12-18 22:06:50 +0100

    pysb0002

--- petabtests/cases/v2.0.0/pysb/0002/0002.py
+++ petabtests/cases/v2.0.0/pysb/0002/0002.py
@@ -1,8 +1,7 @@
 from inspect import cleandoc
-from math import nan
 
-import pandas as pd
-from petab.v1.C import *
+from petab.v2 import Problem
+from petab.v2.C import *
 
 from petabtests import DEFAULT_PYSB_FILE, PetabTestCase, analytical_a
 
@@ -24,61 +23,46 @@ mass action kinetics.
 """)
 
 # problem --------------------------------------------------------------------
+problem = Problem()
 
-condition_df = pd.DataFrame(
-    data={
-        CONDITION_ID: ["c0", "c1"],
-        "a0": [0.8, 0.9],
-        "b0": [nan, nan],
-    }
-).set_index([CONDITION_ID])
+problem.add_condition("c0", a0=(VT_INITIAL, 0.8))
+problem.add_condition("c1", a0=(VT_INITIAL, 0.9))
 
-measurement_df = pd.DataFrame(
-    data={
-        OBSERVABLE_ID: ["obs_a"] * 4,
-        SIMULATION_CONDITION_ID: ["c0", "c0", "c1", "c1"],
-        TIME: [0, 10, 0, 10],
-        MEASUREMENT: [0.7, 0.1, 0.8, 0.2],
-    }
+problem.add_experiment("e0", 0, "c0")
+problem.add_experiment("e1", 0, "c1")
+
+problem.add_observable("obs_a", "A", noise_formula=1)
+
+problem.add_measurement("obs_a", "e0", time=0, measurement=0.7)
+problem.add_measurement("obs_a", "e0", time=10, measurement=0.1)
+problem.add_measurement("obs_a", "e1", time=0, measurement=0.8)
+problem.add_measurement("obs_a", "e1", time=10, measurement=0.2)
+
+problem.add_parameter(
+    "k1", lb=0, ub=10, nominal_value=0.8, scale=LIN, estimate=True
 )
-
-observable_df = pd.DataFrame(
-    data={
-        OBSERVABLE_ID: ["obs_a"],
-        OBSERVABLE_FORMULA: ["A"],
-        NOISE_FORMULA: [1],
-    }
-).set_index([OBSERVABLE_ID])
-
-parameter_df = pd.DataFrame(
-    data={
-        PARAMETER_ID: ["k1", "k2"],
-        PARAMETER_SCALE: [LIN] * 2,
-        LOWER_BOUND: [0] * 2,
-        UPPER_BOUND: [10] * 2,
-        NOMINAL_VALUE: [0.8, 0.6],
-        ESTIMATE: [1] * 2,
-    }
-).set_index(PARAMETER_ID)
+problem.add_parameter(
+    "k2", lb=0, ub=10, nominal_value=0.6, scale=LIN, estimate=True
+)
 
 # solutions ------------------------------------------------------------------
 
-simulation_df = measurement_df.copy(deep=True).rename(
+simulation_df = problem.measurement_df.copy(deep=True).rename(
     columns={MEASUREMENT: SIMULATION}
 )
 simulation_df[SIMULATION] = [
     *[analytical_a(t, 0.8, 1, 0.8, 0.6) for t in [0, 10]],
     *[analytical_a(t, 0.9, 1, 0.8, 0.6) for t in [0, 10]],
 ]
-
 case = PetabTestCase(
     id=2,
     brief="Simulation. Two conditions. Numeric parameter override.",
     description=DESCRIPTION,
     model=DEFAULT_PYSB_FILE,
-    condition_dfs=[condition_df],
-    observable_dfs=[observable_df],
-    measurement_dfs=[measurement_df],
+    condition_dfs=[problem.condition_df],
+    observable_dfs=[problem.observable_df],
+    measurement_dfs=[problem.measurement_df],
     simulation_dfs=[simulation_df],
-    parameter_df=parameter_df,
+    experiment_dfs=[problem.experiment_df],
+    parameter_df=problem.parameter_df,
 )
--- test/test_generate.py
+++ test/test_generate.py
@@ -4,6 +4,8 @@ from petabtests.C import CASES_DIR
 import sys
 import subprocess
 
+from petabtests.core import create_case
+
 
 def test_check_cases_up_to_date():
     sys.path.insert(0, CASES_DIR)
@@ -13,3 +15,14 @@ def test_check_cases_up_to_date():
     )
     has_changes = res.returncode
     assert not has_changes, res.stdout.decode()
+
+
+def test_create_case():
+    """Test creating a single test case.
+
+    Mostly for debugging purposes.
+    """
+    format_ = "pysb"
+    version = "v2.0.0"
+    id_ = "0002"
+    create_case(format_=format_, version=version, id_=id_)
