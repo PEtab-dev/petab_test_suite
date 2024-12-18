f4885e4538dd14fd86ad0374efbe4c1a47a9c677
Daniel Weindl <daniel.weindl@uni-bonn.de>
2024-12-18 23:41:14 +0100

    pysb 10+11

--- petabtests/cases/v2.0.0/pysb/0011/0011.py
+++ petabtests/cases/v2.0.0/pysb/0011/0011.py
@@ -1,7 +1,7 @@
 from inspect import cleandoc
 
-import pandas as pd
-from petab.v1.C import *
+from petab.v2 import Problem
+from petab.v2.C import *
 
 from petabtests import PetabTestCase, analytical_a
 
@@ -19,49 +19,30 @@ mass action kinetics.
 """)
 
 # problem --------------------------------------------------------------------
+problem = Problem()
 
-condition_df = pd.DataFrame(data={CONDITION_ID: ["c0"], "B": [2]}).set_index(
-    [CONDITION_ID]
+problem.add_condition("c0", B=(VT_INITIAL, 2))
+
+problem.add_experiment("e0", 0, "c0")
+
+problem.add_observable("obs_a", "A", noise_formula=0.5)
+
+problem.add_measurement("obs_a", "e0", time=0, measurement=0.7)
+problem.add_measurement("obs_a", "e0", time=10, measurement=0.1)
+
+problem.add_parameter(
+    "k1", lb=0, ub=10, nominal_value=0.8, scale=LIN, estimate=True
+)
+problem.add_parameter(
+    "k2", lb=0, ub=10, nominal_value=0.6, scale=LIN, estimate=True
 )
 
-measurement_df = pd.DataFrame(
-    data={
-        OBSERVABLE_ID: ["obs_a", "obs_a"],
-        SIMULATION_CONDITION_ID: ["c0", "c0"],
-        TIME: [0, 10],
-        MEASUREMENT: [0.7, 0.1],
-    }
-)
-
-observable_df = pd.DataFrame(
-    data={
-        OBSERVABLE_ID: ["obs_a"],
-        OBSERVABLE_FORMULA: ["A"],
-        NOISE_FORMULA: [0.5],
-    }
-).set_index([OBSERVABLE_ID])
-
-parameter_df = pd.DataFrame(
-    data={
-        PARAMETER_ID: ["k1", "k2"],
-        PARAMETER_SCALE: [LIN] * 2,
-        LOWER_BOUND: [0] * 2,
-        UPPER_BOUND: [10] * 2,
-        NOMINAL_VALUE: [0.8, 0.6],
-        ESTIMATE: [1] * 2,
-    }
-).set_index(PARAMETER_ID)
-
-mapping_df = pd.DataFrame(
-    data={
-        PETAB_ENTITY_ID: ["A", "B"],
-        MODEL_ENTITY_ID: ["A_() ** compartment", "B_() ** compartment"],
-    }
-).set_index(PETAB_ENTITY_ID)
+problem.add_mapping("A", "A_() ** compartment")
+problem.add_mapping("B", "B_() ** compartment")
 
 # solutions ------------------------------------------------------------------
 
-simulation_df = measurement_df.copy(deep=True).rename(
+simulation_df = problem.measurement_df.copy(deep=True).rename(
     columns={MEASUREMENT: SIMULATION}
 )
 simulation_df[SIMULATION] = [
@@ -73,10 +54,11 @@ case = PetabTestCase(
     brief="Simulation. InitialAssignment to species overridden.",
     description=DESCRIPTION,
     model="conversion_modified_pysb.py",
-    condition_dfs=[condition_df],
-    observable_dfs=[observable_df],
-    measurement_dfs=[measurement_df],
+    condition_dfs=[problem.condition_df],
+    observable_dfs=[problem.observable_df],
+    measurement_dfs=[problem.measurement_df],
     simulation_dfs=[simulation_df],
-    parameter_df=parameter_df,
-    mapping_df=mapping_df,
+    parameter_df=problem.parameter_df,
+    mapping_df=problem.mapping_df,
+    experiment_dfs=[problem.experiment_df],
 )
