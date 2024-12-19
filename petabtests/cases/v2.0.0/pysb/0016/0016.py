e68404162e238436781a690c790d364985afe3ce
Daniel Weindl <daniel.weindl@uni-bonn.de>
2024-12-19 09:39:59 +0100

    ..

--- petabtests/cases/v2.0.0/pysb/0016/0016.py
+++ petabtests/cases/v2.0.0/pysb/0016/0016.py
@@ -1,7 +1,7 @@
 from inspect import cleandoc
 
-import pandas as pd
-from petab.v1.C import *
+from petab.v2 import Problem
+from petab.v2.C import *
 
 from petabtests import (
     DEFAULT_PYSB_FILE,
@@ -25,45 +25,27 @@ A simple conversion reaction `A <=> B` in a single compartment, following
 mass action kinetics.
 """)
 # problem --------------------------------------------------------------------
-
-condition_df = pd.DataFrame(
-    data={
-        CONDITION_ID: ["c0"],
-    }
-).set_index([CONDITION_ID])
-
-measurement_df = pd.DataFrame(
-    data={
-        OBSERVABLE_ID: ["obs_a", "obs_b"],
-        SIMULATION_CONDITION_ID: ["c0", "c0"],
-        TIME: [10, 10],
-        MEASUREMENT: [0.2, 0.8],
-    }
+problem = Problem()
+problem.add_observable("obs_a", "A", noise_formula=0.5, transform=LIN)
+problem.add_observable("obs_b", "B", noise_formula=0.7, transform=LOG)
+problem.add_measurement("obs_a", "", time=10, measurement=0.2)
+problem.add_measurement("obs_b", "", time=10, measurement=0.8)
+problem.add_parameter(
+    "a0", lb=0, ub=10, nominal_value=1, scale=LIN, estimate=True
 )
-
-observable_df = pd.DataFrame(
-    data={
-        OBSERVABLE_ID: ["obs_a", "obs_b"],
-        OBSERVABLE_FORMULA: ["A", "B"],
-        OBSERVABLE_TRANSFORMATION: [LIN, LOG],
-        NOISE_FORMULA: [0.5, 0.7],
-    }
-).set_index([OBSERVABLE_ID])
-
-parameter_df = pd.DataFrame(
-    data={
-        PARAMETER_ID: ["a0", "b0", "k1", "k2"],
-        PARAMETER_SCALE: [LIN] * 4,
-        LOWER_BOUND: [0] * 4,
-        UPPER_BOUND: [10] * 4,
-        NOMINAL_VALUE: [1, 0, 0.8, 0.6],
-        ESTIMATE: [1] * 4,
-    }
-).set_index(PARAMETER_ID)
+problem.add_parameter(
+    "b0", lb=0, ub=10, nominal_value=0, scale=LIN, estimate=True
+)
+problem.add_parameter(
+    "k1", lb=0, ub=10, nominal_value=0.8, scale=LIN, estimate=True
+)
+problem.add_parameter(
+    "k2", lb=0, ub=10, nominal_value=0.6, scale=LIN, estimate=True
+)
 
 # solutions ------------------------------------------------------------------
 
-simulation_df = measurement_df.copy(deep=True).rename(
+simulation_df = problem.measurement_df.copy(deep=True).rename(
     columns={MEASUREMENT: SIMULATION}
 )
 simulation_df[SIMULATION] = [
@@ -76,9 +58,9 @@ case = PetabTestCase(
     brief="Simulation. Observable transformation log.",
     description=DESCRIPTION,
     model=DEFAULT_PYSB_FILE,
-    condition_dfs=[condition_df],
-    observable_dfs=[observable_df],
-    measurement_dfs=[measurement_df],
+    condition_dfs=[problem.condition_df],
+    observable_dfs=[problem.observable_df],
+    measurement_dfs=[problem.measurement_df],
     simulation_dfs=[simulation_df],
-    parameter_df=parameter_df,
+    parameter_df=problem.parameter_df,
 )
